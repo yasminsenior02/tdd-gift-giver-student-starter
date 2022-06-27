40f87de8a954e3c685a5dafc7e29e3fb5f91dcd6
yasminsenior02 <yasminsenior02@gmail.com>
2022-06-27 14:29:21 -0700

    step 1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const app = require("./app")
+const app = require("./app");
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log(`🚀 Server listening at http://localhost:${port}`)
-})
+  console.log(`🚀 Server listening at http://localhost:${port}`);
+});
